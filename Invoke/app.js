d79482266df4d3e0e13adb1732cc0e83cad528ef
Komal Verma <komalverma22012005@gmail.com>
2025-11-02 12:02:41 +0530

    added: tool calling

--- Invoke/app.js
+++ Invoke/app.js
@@ -1,12 +1,26 @@
-import "dotenv/config";
-import OpenAI from "openai";
-const client = new OpenAI({
-    apiKey: process.env.GROQ_API_KEY,
-    baseURL: "https://api.groq.com/openai/v1",
-});
+import "dotenv/config"
+import openAi from "openai"
 
-const response = await client.responses.create({
-    model: "openai/gpt-oss-20b",
-    input: "Explain the importance of fast language models",
-});
-console.log(response);
+const client=new openAi({
+    apikey:process.env.GROQ_API_KEY,
+    baseurl:""
+})
+
+async function main(){
+    const response=await client.chat.completions.create({
+       model:"",
+       temperature: 0.8,
+       top_p: 0.2 ,
+       max_completion_tokens: 100,
+  frequency_penalty: 1,
+presence_penalty: 1,
+       messages:[{
+        role:"user",
+        content:""
+       }]        
+    })
+    console.log(response.choices[0].message.content);
+    
+}
+
+main();
